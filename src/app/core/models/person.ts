4e7d541fb0eb3e8716f33dc17ccf838d6606fd7a
Le Quang Duy <quangduynb00@gmail.com>
2021-12-30 16:22:23 +0700

    Beta:

--- src/app/core/models/person.ts
+++ src/app/core/models/person.ts
@@ -1,16 +1,3 @@
-// export class Person {
-//   id?: number;
-//   firstName: string;
-//   age: number;
-//   job: string;
-//
-//   constructor(id: number = null, firstName: string = '', age: number = 0, job: string = '') {
-//     this.id = id;
-//     this.firstName = firstName;
-//     this.age = age;
-//     this.job = job;
-//   }
-// }
 export class Person {
   idRecord: string;
   patientId: string;
